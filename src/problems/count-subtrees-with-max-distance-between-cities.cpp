b2b2e21637f14efa88e605ebe9782dedd9f9aca9
1290185595 <t1290185595@qq.com>
2023-03-19 10:29:35 +0800

    。

---
+++ src/problems/count-subtrees-with-max-distance-between-cities.cpp
@@ -0,0 +1,19 @@
+#include "library.h"
+
+/**
+ * <a href="https://leetcode.cn/problems/count-subtrees-with-max-distance-between-cities/">1617. 统计子树中城市之间最大距离</a>
+ */
+
+class Solution {
+public:
+    vector<int> countSubgraphsForEachDiameter(int n, vector<vector<int>>& edges) {
+        return {};
+    }
+};
+
+
+int main() {
+    test(&Solution::countSubgraphsForEachDiameter, {
+            "4 [[1,2],[2,3],[2,4]]",
+    });
+}
